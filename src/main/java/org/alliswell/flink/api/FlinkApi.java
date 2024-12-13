6e25c1113d7f5c517cd085d0ec4ca81bc68396cb
wanshaobo <1647579959@qq.com>
2024-12-13 18:31:17 +0800

    V2

--- src/main/java/org/alliswell/flink/api/FlinkApi.java
+++ src/main/java/org/alliswell/flink/api/FlinkApi.java
@@ -23,7 +23,6 @@ public class FlinkApi {
         // 判断两个集合是否相等
         if (map1.keySet().equals(map2.keySet())) {
             System.out.println("两个集合的元素完全相同！");
-            System.out.println("测试区别");
         } else {
             System.out.println("两个集合的元素不同！");
         }
